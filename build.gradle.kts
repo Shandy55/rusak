97a8908e21421f2333f41c06da8b15ba148674d7
shandy <shandyhandika75@gmail.com>
2023-11-07 23:19:28 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:2.4.2-alpha09")
+    }
+}
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.4.2" apply false
+
+}
+
+
+
+
